3c1acbd677099f5dceaab116e6c8df9ed9912ce8
Alov Maxim <alovmax@yandex.ru>
2013-07-14 00:55:51 +0400

    Added engine header.

---
+++ include/engine.h
@@ -0,0 +1,21 @@
+/*
+ * engine.h
+ *
+ *  Created on: 13.07.2013
+ *      Author: dla
+ */
+
+#ifndef ENGINE_H_
+#define ENGINE_H_
+
+
+namespace mw {
+
+class Engine {
+public:
+  void run() const;
+};
+
+}  // namespace mw
+
+#endif /* ENGINE_H_ */
